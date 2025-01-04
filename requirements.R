1b798d92041d160c97760f2aae0b376ba241b1b8
Kristopher Wilson <91929102+krisawilson@users.noreply.github.com>
2025-01-04 18:44:20 -0500

    Add files via upload

---
+++ requirements.R
@@ -0,0 +1,11 @@
+# purpose: necessary packages for the workshop 
+
+# one way: use the default install.packages() function.
+# instead, going to use 'pak', since it's faster and more efficient:
+install.packages("pak")
+
+# now, here are the necessary packages to install:
+packs <- c("tidyverse", 
+           "FNN", 
+           "tidytuesdayR")
+pak::pkg_install(pkg = packs)
